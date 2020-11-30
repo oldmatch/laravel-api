bf7eb4f969817902c8ccccbaa00cf5800f11643f
oldmatch <59426726+oldmatch@users.noreply.github.com>
2020-11-30 11:14:09 +0800

    init

---
+++ app/Admin/Controllers/ArticleController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+use App\Models\Article;
+use Encore\Admin\Controllers\AdminController;
+use Encore\Admin\Form;
+use Encore\Admin\Grid;
+use Encore\Admin\Show;
+
+class ArticleController extends AdminController
+{
+    /**
+     * Title for current resource.
+     *
+     * @var string
+     */
+    protected $title = 'Article';
+
+    /**
+     * Make a grid builder.
+     *
+     * @return Grid
+     */
+    protected function grid()
+    {
+        $grid = new Grid(new Article());
+
+        $grid->column('id', __('Id'));
+        $grid->column('title', __('标题'));
+        $grid->column('author', __('作者'));
+        $grid->column('article_content', __('内容'));
+        $grid->column('created_at', __('创建时间'));
+        $grid->column('updated_at', __('更新时间'));
+        //$grid->column('deleted_at', __('Deleted at'));
+
+        return $grid;
+    }
+
+    /**
+     * Make a show builder.
+     *
+     * @param mixed $id
+     * @return Show
+     */
+    protected function detail($id)
+    {
+        $show = new Show(Article::findOrFail($id));
+
+        $show->field('id', __('Id'));
+        $show->field('title', __('标题'));
+        $show->field('author', __('作者'));
+        $show->field('article_content', __('文章内容'));
+        $show->field('created_at', __('创建时间'));
+        $show->field('updated_at', __('更新时间'));
+        //$show->field('deleted_at', __('Deleted at'));
+
+        return $show;
+    }
+
+    /**
+     * Make a form builder.
+     *
+     * @return Form
+     */
+    protected function form()
+    {
+        $form = new Form(new Article());
+
+        $form->text('title', __('标题'));
+        $form->text('author', __('作者'));
+        $form->textarea('article_content', __('文章内容'));
+
+        return $form;
+    }
+}
---
+++ app/Models/Article.php
@@ -0,0 +1,9 @@
+<?php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Article extends Model {
+    //此处填写表名
+    protected $table = 'article';
+}
